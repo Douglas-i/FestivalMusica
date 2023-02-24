4ee3d0022aa6fd5e8c887a3c74be8fd56bec52bd
Douglas-i <hdouglasu@gmail.com>
2023-02-24 13:03:30 -0600

    Conversion de imagenes: Webp, imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {    
 
@@ -12,10 +19,37 @@ function css(done) {
     done();//Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes(done) {
+    const opciones = {
+        optmizacionLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    
+    done();
+}
+
+function versionWebp(done) {
+    
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
