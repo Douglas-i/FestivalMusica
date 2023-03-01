2aaf65bf0669b8bb4809a8fb765065e64f4a943f
Douglas-i <hdouglasu@gmail.com>
2023-03-01 16:20:39 -0600

    Scroll y Barra fija

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,37 @@ document.addEventListener('DOMContentLoaded', function(){
 })
 
 function iniciarApp() {
+    navegacionFija();
     crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija(){
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function() {
+        if(sobreFestival.getBoundingClientRect().top < 0 ){
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
+}
+
+function scrollNav() {
+    const enlances = document.querySelectorAll('.navegacion-principal a');
+    enlances.forEach( enlances => {
+        enlances.addEventListener('click', function(e) {
+            e.preventDefault();
+            const seccionScroll = e.target.attributes.href.value
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        })
+    })
 }
 
 function crearGaleria() {
